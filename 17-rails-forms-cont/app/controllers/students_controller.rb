4578be927a13539b0f3890013761546ebbc26aff
kingcons <brit@kingcons.io>
2019-08-13 13:42:24 -0400

    Add finished Rails Forms code.

--- 17-rails-forms-cont/app/controllers/students_controller.rb
+++ 17-rails-forms-cont/app/controllers/students_controller.rb
@@ -3,17 +3,45 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
   def new
+    @student = Student.new
   end
 
   def create
-    @student = Student.create(student_params)
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
     redirect_to students_path
   end
 
   private
 
   def student_params
-    params.permit(:name, :cohort_id)
+    params.require(:student).permit(:name, :cohort_id)
   end
 end
--- 17-rails-forms-cont/app/models/student.rb
+++ 17-rails-forms-cont/app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :cohort
+
+  validates :name, presence: true, uniqueness: true
 end
